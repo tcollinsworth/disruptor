aa2f015b84e43cc7d39445447bcfce6e43b54c79
troy <troycollinsworth@gmail.com>
2020-02-21 20:50:11 -0500

    added test of creating and passing numpy as pointer to process

---
+++ tests/test_numpy_pointers.py
@@ -0,0 +1,38 @@
+import pytest
+import numpy as np
+from ctypes import Structure, POINTER, pointer, byref, cast, c_float
+import multiprocessing
+
+ITEM_CNT = 3
+VECTOR_DIMENSIONS = 2
+
+def p1(np_ptr):
+    print(f'w1 np_ptr {np_ptr}')
+
+    a1 = np.ctypeslib.as_array(np_ptr)
+    print(f'w1 type(a1) {type(a1)}')
+    print(f'w1 a1 {a1}')
+
+def test_struct():
+    a = 2 * np.random.random_sample((ITEM_CNT, VECTOR_DIMENSIONS)) - 1
+    print()
+    print(f'type(a) {type(a)}')
+    print(a)
+    print(a.ctypes.data)
+
+    ptr = np.ctypeslib.as_ctypes(a)
+    print(f'ptr {ptr}')
+
+    a1 = np.ctypeslib.as_array(ptr)
+    print(f'type(a1) {type(a1)}')
+    print(f'a1 {a1}')
+
+    w1 = multiprocessing.Process(name='w1', target=p1, args=(ptr,))
+    w1.start()
+    w1.join()
+
+#https://pymotw.com/2/multiprocessing/communication.html
+#https://docs.scipy.org/doc/numpy-1.14.0/reference/routines.ctypeslib.html
+#https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.ctypes.html
+#https://stackoverflow.com/a/53387364
+#https://stackoverflow.com/a/3671889
